c08ab5e82ed0c62da43b13c467665e75ac67c012
Seth Bingaman <seth.bingaman@wsu.edu>
2026-01-22 16:44:17 -0800

    f you man

---
+++ Project2/.rigel_lopper/_ide/workspace_journal.py
@@ -0,0 +1,8 @@
+# 2026-01-22T16:42:07.531718800
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path=".rigel_lopper")
+
+vitis.dispose()
+
--- Project2/_ide/workspace_journal_50020.py
+++ Project2/_ide/workspace_journal_50020.py
@@ -1,4 +1,4 @@
-# 2026-01-22T16:43:52.224706600
+# 2026-01-21T23:38:35.860127500
 import vitis
 
 client = vitis.create_client()
